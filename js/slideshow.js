6e04086bdf6bf80c152cfbf8d0a46e9e3e1861ae
Gary Trakhman <gary.trakhman@gmail.com>
2013-07-14 12:30:23 -0400

    more slides

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,5 +1,15 @@
 angular.module('slideshow', []).directive('clojure', function() {
-  var l = '<pre><code class=\"clojure\">';
+  var l = '<pre><code class=clojure>';
+  var r = '</code></pre>';
+  return {
+    restrict: 'E',
+    link: function(scope, element, attrs) {
+      var html = element.html();
+      element.html(l+html+r);
+    }
+  }
+}).directive('auto', function() {
+  var l = '<pre><code>';
   var r = '</code></pre>';
   return {
     restrict: 'E',
